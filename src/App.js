5d21f47ab3178c4398bc3d4404b92954a810bd6b
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-25 15:56:57 -0300

    Requisito 1 completo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
 function App() {
   return (
