dae1a3feb509a956c8ba5b3cb17506e5d7cf5545
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-25 17:17:26 -0300

    Requisito 3 completo

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 // import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
-
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route path="/shoppingCart" component={ ShoppingCart } />
       </Switch>
     </BrowserRouter>
   );
 }
-
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" />
+        <label htmlFor="input-search">
+          <input
+            type="text"
+            id="input-search"
+          />
+        </label>
+        <Link to="/shoppingCart" data-testid="shopping-cart-button">
+          <img
+            src="https://files.slack.com/files-pri/TMDDFEPFU-F02JWM7D9EZ/shoppingcarticon.png"
+            alt="cart-img"
+          />
+        </Link>
         <h4 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h4>
@@ -12,5 +24,4 @@ class Home extends React.Component {
     );
   }
 }
-
 export default Home;
