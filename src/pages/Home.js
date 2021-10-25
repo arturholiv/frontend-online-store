07d14711d2a2ce923641fab7d87ba8981abc16e1
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-10-25 18:37:59 -0300

    Requisito 4 completo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.getCategoriesList = this.getCategoriesList.bind(this);
+
+    this.state = {
+      categoriesList: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getCategoriesList();
+  }
+
+  async getCategoriesList() {
+    const list = await getCategories();
+    this.setState({ categoriesList: list });
+  }
+
   render() {
+    const { categoriesList } = this.state;
     return (
       <div>
         <label htmlFor="input-search">
@@ -13,13 +34,21 @@ class Home extends React.Component {
         </label>
         <Link to="/shoppingCart" data-testid="shopping-cart-button">
           <img
-            src="https://files.slack.com/files-pri/TMDDFEPFU-F02JWM7D9EZ/shoppingcarticon.png"
-            alt="cart-img"
+            src="https://img.icons8.com/ios-glyphs/30/000000/shopping-cart--v1.png"
+            alt="cart"
           />
         </Link>
         <h4 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h4>
+        <section>
+          <ul>
+            { categoriesList
+              .map((categories) => (
+                <li data-testid="category" key={ categories.id }>{ categories.name }</li>
+              )) }
+          </ul>
+        </section>
       </div>
     );
   }
